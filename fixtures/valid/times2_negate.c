97285b7378817bc0ac629ac555d7b771dfa53376
Stephen Riley <jstephen.riley@yahoo.com>
2025-05-05 14:08:09 -0700

    updates on some test fixtures

---
+++ fixtures/valid/times2_negate.c
@@ -0,0 +1,7 @@
+int times2(int i) {
+    return 2*i;
+}
+
+int main(void) {
+    return ~(times2(-1));
+}
--- src/CodeGen/AbstractAsm/ProgramGen.cs
+++ src/CodeGen/AbstractAsm/ProgramGen.cs
@@ -5,5 +5,7 @@ public record ProgramGen() : IAbstractAsm
 {
     public void Emit(TextWriter stream) => stream.WriteLine(EmitString());
 
-    public string EmitString() => "    .section __TEXT,__text,regular,pure_instructions";
+    public string EmitString() =>
+        "    .section __TEXT,__text,regular,pure_instructions\n" +
+        "    .align 2";
 }
