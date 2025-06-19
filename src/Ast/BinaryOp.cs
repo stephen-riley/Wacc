8c6ac3f17c1de8303d65af600d2e03bb62bc9321
Stephen Riley <jstephen.riley@yahoo.com>
2025-06-19 13:44:24 -0700

    fix operator precedence; now passes all ch4 tests w/ extra credit

--- src/Ast/BinaryOp.cs
+++ src/Ast/BinaryOp.cs
@@ -16,23 +16,42 @@ public class BinaryOp(string op, IAstNode lExpr, IAstNode rExpr) : IAstNode
 
     // Reference: https://en.cppreference.com/w/c/language/operator_precedence
     public static readonly Dictionary<TokenType, int> Precedence = new() {
+        // level 3
         { MulSign, 50 },
         { DivSign, 50 },
         { ModSign, 50 },
+
+        // level 4
         { PlusSign, 45 },
         { MinusSign, 45 },
+
+        // level 5
         { BitwiseLeft, 40 },
         { BitwiseRight, 40 },
-        { BitwiseAnd, 35 },
+
+        // level 6
         { LessThan,35 },
         { LessOrEqual, 35 },
         { GreaterThan, 35 },
         { GreaterOrEqual, 35 },
-        { BitwiseXor, 34 },
-        { BitwiseOr, 33} ,
+
+        // level 7
         { EqualTo, 30 },
         { NotEqualTo, 30 },
+
+        // level 8
+        { BitwiseAnd, 25 },
+
+        // level 9
+        { BitwiseXor, 20 },
+
+        // level 10
+        { BitwiseOr, 15 },
+
+        // level 11
         { LogicalAnd, 10 },
+
+        // level 12
         { LogicalOr, 5 },
     };
 
